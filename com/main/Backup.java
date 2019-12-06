bdb8c1bfdb14e71a85867c84fc2218d8d8279614
Ed Werner <android.devops@gmail.com>
2019-12-05 22:35:29 -0500

    adds create table logging

--- com/main/Backup.java
+++ com/main/Backup.java
@@ -1,20 +1,77 @@
 package com.main;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Backup.
+ */
 public class Backup {
-	
+
 	private final String url;
 	private final String username;
 	private final String password;
 	private Connection conn;
+	private Statement stmnt;
 
-	// backup constructor
+	/**
+	 * Instantiates a new backup.
+	 */
 	public Backup() {
 		url = "jdbc:mysql://localhost:3306/travel?useSSL=false";
 		username = "root";
 		password = "Gv3rn1ca";
+	}
+
+	/**
+	 * Fetch table names.
+	 *
+	 * @return the string array list
+	 */
+	public ArrayList<String> fetchTable(String tableName) {
+		ArrayList<String> tableNames = new ArrayList<String>();
+		String SHOW_QUERY = "";
+		
+		if (tableName == null) {
+			SHOW_QUERY = "SHOW TABLES";
+		} else {
+			SHOW_QUERY = "SHOW CREATE TABLE " + tableName;
+		}
+
+		try {
+			stmnt = conn.createStatement();
+		} catch (SQLException e) {
+			try {
+				// throw dlexception and pass error info
+				String[] errorInfo = { String.valueOf(e.getStackTrace()) };
+				throw new DLException(e, errorInfo);
+			} catch (DLException e1) {
+				System.out.println("There was an error completing an operation.");
+			}
+		}
+
+		try {
+			// execute string query
+			final ResultSet rs = stmnt.executeQuery(SHOW_QUERY);
+			while (rs.next()) {
+				tableNames.add(rs.getString(1));
+				tableNames.add(rs.getString(2));
+			}
+		} catch (SQLException e) {
+			try {
+				// throw dlexception and pass error info
+				String[] errorInfo = { String.valueOf(e.getStackTrace()) };
+				throw new DLException(e, errorInfo);
+			} catch (DLException e1) {
+				System.out.println("There was an error completing an operation.");
+			}
+		}
+		return tableNames;
 	}
 
 	/**
--- com/main/Main.java
+++ com/main/Main.java
@@ -1,7 +1,61 @@
 package com.main;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+
+	private static Backup backup;
+	private static String name = "";
+	private static Scanner scanner;
+
 	public static void main(String[] args) {
-		
+
+		// create new backup instance
+		backup = new Backup();
+
+		// connect to database
+		backup.connect();
+
+		// fetch table names
+		ArrayList<String> tableNames = backup.fetchTable(null);
+		for (String name : tableNames) {
+			System.out.println(name);
+		}
+		System.out.println("----------------------------------------------------");
+
+		System.out.println("Please enter a valid table name");
+
+		System.out.println("----------------------------------------------------");
+
+		// Enter data using Scanner
+		scanner = new Scanner(System.in);
+
+		name = scanner.next();
+
+		tableExists(backup.fetchTable(name));
+	}
+
+	public static ArrayList<String> tableExists(ArrayList<String> tableData) {
+
+		ArrayList<String> showTable = new ArrayList<String>();
+		// check if queried table exists
+		if (tableData.size() > 0) {
+			System.out.println(tableData);
+			System.out.println("----------------------------------------------------");
+//			showTable = tableExists(backup.fetchTable(null));
+			
+			// close database connection
+			backup.close();
+		} else {
+			System.out.println("----------------------------------------------------");
+			System.out.println("Please enter a valid table name");
+			System.out.println("----------------------------------------------------");
+			name = scanner.next();
+
+			// recursive method call
+			showTable = tableExists(backup.fetchTable(name));
+		}
+		return showTable;
 	}
 }
